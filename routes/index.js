c03d1e4d65afc8577f8b4a8f7b6401053c3fa400
Glitch (wallstreetbets) <none>
2018-04-14 23:35:07 +0000

    Updated with Glitch

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,70 @@
+// const testData = require(process.cwd() + '/testData.js');
 const fetch = require('node-fetch');
 
 module.exports = function (app, serverData, aWss) {
+  
+  function sendMessage(recipient, msg) {
+    if (recipient.readyState !== recipient.CLOSED) {
+      recipient.send(JSON.stringify(msg));
+    } else {
+      console.log('Connection is closed');
+    }
+  }
+  
+  function fetchStockData (symbol) {    
+    return new Promise((resolve, reject) => {
+    let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+
+            symbol + '&outputsize=full&apikey=' + process.env.STOCK_API_KEY;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        // let json = testData.msft;
+        let weeklyData = [], minClose, maxClose, stockData = {};
+
+        Object.keys(json["Time Series (Daily)"]).forEach(function (key) {
+          let close = +json['Time Series (Daily)'][key]['5. adjusted close'];
+          let date = new Date(key).toString();
+          if (minClose === undefined || minClose > close) {
+            minClose = close;
+          }
+          if (maxClose === undefined || maxClose < close) {
+            maxClose = close;
+          }
+          weeklyData.push({date: date, close: close});
+        });
+
+        stockData.stock = symbol;
+        stockData.weeklyData = weeklyData;
+        stockData.length = weeklyData.length;
+        stockData.minClose = minClose;
+        stockData.maxClose = maxClose;
+
+        return resolve(stockData);
+
+      });
+    }).catch(err => console.log('Error:', err));
+  }
+  
+  function addStock(recipiant, symbol) {
+    sendMessage(recipiant, {command: 'ADD', stock: symbol});
+    fetchStockData(symbol).then(data => {
+      let response = {
+        command: 'UPDATE',
+        stock: symbol,
+        data: data
+      }
+      setTimeout(()=>{
+        sendMessage(recipiant,response);
+      }, (Math.random() * 5) * 1000);
+    }).catch(err => {
+      let response = {
+        command: 'ERROR',
+        error: err
+      }
+      sendMessage(recipiant,response);
+    });
+  }
   
   app.get('/', function(req, res, next) {
     res.render('index');
@@ -14,56 +78,29 @@ module.exports = function (app, serverData, aWss) {
     ws.on('message', function(msg) {
       msg = JSON.parse(msg);
       
-      if (msg.command === 'HEARTBEAT' || msg.command === null) {
-        return ws.send(JSON.stringify({type: null}))
+      if (msg.command === 'PING' || msg.command === null) {
+        return ws.send(JSON.stringify({command: "PONG"}));
       }
       
-      if (msg.command === 'ADD') {
-        serverData.trackedSymbols.push(msg.data);
-      }
-      
-      if (msg.command === 'REMOVE') {
-        serverData.trackedSymbols.splice(serverData.trackedSymbols.indexOf(msg.data));
-        aWss.clients.forEach(client => {
-          client.send(JSON.stringify({type: 'REMOVE', symbol: msg.data}))
+      if (msg.command === 'STARTUP') {
+        serverData.trackedSymbols.forEach(symbol => {
+          addStock(ws, symbol);
         });
       }
       
-      serverData.trackedSymbols.forEach(symbol => {
-        let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+
-            symbol + '&outputsize=full&apikey=' + process.env.STOCK_API_KEY;
-
-        fetch(url)
-        .then(res => res.json())
-        .then(json => {
-          let weeklyData = [], minClose, maxClose, stockData = {};
-          
-          Object.keys(json["Time Series (Daily)"]).forEach(function (key) {
-            let close = +json['Time Series (Daily)'][key]['5. adjusted close'];
-            let date = new Date(key).toString();
-            if (minClose === undefined || minClose > close) {
-              minClose = close;
-            }
-            if (maxClose === undefined || maxClose < close) {
-              maxClose = close;
-            }
-            weeklyData.push({date: date, close: close});
-          });
-          
-          stockData.stock = symbol;
-          stockData.weeklyData = weeklyData;
-          stockData.length = weeklyData.length;
-          stockData.minClose = minClose;
-          stockData.maxClose = maxClose;
-          stockData.minDate = weeklyData[weeklyData.length - 1].date;
-          stockData.maxDate = weeklyData[0].date;
-          
-          aWss.clients.forEach(client => {
-            client.send(JSON.stringify({type: 'UPDATE', symbol: symbol, stockData: stockData}))
-          });
-        }).catch(err => console.log('Error:', err));
-      });
+      if (msg.command === 'ADD') {
+        serverData.trackedSymbols.push(msg.stock);
+        aWss.clients.forEach(client => {
+          addStock(client, msg.stock);
+        });
+      }
       
+      if (msg.command === 'REMOVE') {
+        serverData.trackedSymbols.splice(serverData.trackedSymbols.indexOf(msg.stock));
+        aWss.clients.forEach(client => {
+          sendMessage(client, {command: 'REMOVE', stock: msg.stock});
+        });
+      }
     });
 
     ws.on('close', function (msg) {
--- server.js
+++ server.js
@@ -7,10 +7,11 @@ let aWss = expressWs.getWss('/');
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/views");
 
+app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/controllers'));
 
 let serverData = {
-  trackedSymbols: ['A']
+  trackedSymbols: ['MSFT', 'AAPL', 'BOX']
 }
 
 const routes = require('./routes/index.js');
